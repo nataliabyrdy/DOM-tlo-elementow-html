c30bc2571a4f06b6d29203ebdb10bda87b2a3048
nataliabyrdy <nataliabyrdy@onet.pl>
2023-10-02 21:12:23 +0200

    implementacja kodu

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("test");
+
+const setBg = () => {
+    let p1 = document.querySelector('.first-p');
+    let p2 = document.querySelector('.second-p');
+
+    p1.classList.toggle('bg-red');
+    p2.classList.toggle('bg-yellow');
+}
+
+let btnSetColor = document.getElementById('set-color');
+btnSetColor.addEventListener('click', setBg);
